53fe675043cec0900aebd71e370dc0769647aafc
Paul Campbell <pcampbell@kemitix.net>
2017-08-13 22:15:49 +0100

    PassthroughPrintStreamInterceptorTest: remove unused imports

--- src/test/java/net/kemitix/interceptor/printstream/PassthroughPrintStreamInterceptorTest.java
+++ src/test/java/net/kemitix/interceptor/printstream/PassthroughPrintStreamInterceptorTest.java
@@ -31,9 +31,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
