947e800c88205d3bd97e8f2f705d8b88a9282872
Paul Campbell <pcampbell@kemitix.net>
2018-03-29 17:46:02 +0100

    More tests

--- src/test/java/net/kemitix/wrapper/printstream/ByteBufferSegmentTest.java
+++ src/test/java/net/kemitix/wrapper/printstream/ByteBufferSegmentTest.java
@@ -23,6 +23,44 @@ public class ByteBufferSegmentTest {
     }
 
     @Test
+    public void canScanSingleByteFromBufferIntoConsumer() {
+        //given
+        final byte[] bytes = {'a', 'b', 'c', 'd'};
+        final List<Byte> capture = new ArrayList<>();
+        //when
+        new ByteBufferSegment(bytes, 1, 1)
+                .forEach(capture::add);
+        //then
+        assertThat(capture).containsExactly((byte) 'b');
+    }
+
+    @Test
+    public void canScanZeroBytesFromBufferIntoConsumer() {
+        //given
+        final byte[] bytes = {'a', 'b', 'c', 'd'};
+        final List<Byte> capture = new ArrayList<>();
+        //when
+        new ByteBufferSegment(bytes, 1, 0)
+                .forEach(capture::add);
+        //then
+        assertThat(capture).isEmpty();
+    }
+
+    @Test
+    public void whenAttemptToGoBeyondBufferEndThenThrowException() {
+        //given
+        final byte[] bytes = {'a', 'b', 'c', 'd'};
+        final List<Byte> capture = new ArrayList<>();
+        //then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    new ByteBufferSegment(bytes, 2, 3)
+                            .forEach(capture::add);
+                });
+        assertThat(capture).isEmpty();
+    }
+
+    @Test
     public void requiredLengthGreaterThenOrEqualToZero() {
         assertThatExceptionOfType(IndexOutOfBoundsException.class)
                 .isThrownBy(() -> {
