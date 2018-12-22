9903ffac74d4b79a8e820376e774b13e3e080f55
Paul Campbell <pcampbell@kemitix.net>
2018-12-22 18:00:11 +0000

    [jenkins] remove codacy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,12 +8,6 @@ pipeline {
             steps {
                 withMaven(maven: 'maven', jdk: 'JDK 1.8') {
                     sh "${mvn} clean compile checkstyle:checkstyle pmd:pmd test"
-                    // Code Coverage to Codacy
-                    sh "${mvn} jacoco:report com.gavinmogan:codacy-maven-plugin:coverage " +
-                            "-DcoverageReportFile=target/site/jacoco/jacoco.xml " +
-                            "-DprojectToken=`$JENKINS_HOME/codacy/token` " +
-                            "-DapiToken=`$JENKINS_HOME/codacy/apitoken` " +
-                            "-Dcommit=`git rev-parse HEAD`"
                     // Code Coverage to Jenkins
                     jacoco exclusionPattern: '**/*{Test|IT|Main|Application|Immutable}.class'
                     // PMD to Jenkins
